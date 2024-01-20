6f424ca5be029dffa5cd80d316f522d9b0da0f84
Edghi <edghimakoll@gmail.com>
2024-01-21 00:24:35 +0100

    Adding events app

--- website_api/settings/extra.py
+++ website_api/settings/extra.py
@@ -1,20 +1,15 @@
 REST_FRAMEWORK = {
-    'EXCEPTION_HANDLER': 'exceptions.handler.api_exception_handler',
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "EXCEPTION_HANDLER": "exceptions.handler.api_exception_handler",
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
-    'NON_FIELD_ERRORS_KEY': 'message',
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+    "NON_FIELD_ERRORS_KEY": "message",
 }
 
-SWAGGER_SETTINGS = {
-    "SECURITY_DEFINITIONS": {"oauth2": {"type": "Bearer"}}
-}
-
+SWAGGER_SETTINGS = {"SECURITY_DEFINITIONS": {"oauth2": {"type": "Bearer"}}}
