3e45fca17eb27bd72e9729b88186b1f02b22e140
Edghi <edghimakoll@gmail.com>
2024-01-28 00:59:37 +0100

    Update event model

--- events/models/event.py
+++ events/models/event.py
@@ -28,6 +28,10 @@ class Event(models.Model):
     )
     speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
     tags = models.ManyToManyField("EventTag", related_name="events")
+    # TODO: Add field for displying who created the event
+    # created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
