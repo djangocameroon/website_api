c0079d24985bf350c9ead55d950d0d76d1be742c
Steve Yonkeu <yokwejuste@yahoo.com>
2024-01-29 12:00:01 +0100

    feat: updates

--- website_api/routes/main.py
+++ website_api/routes/main.py
@@ -3,6 +3,8 @@ from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,24 +22,26 @@ schema_view = get_schema_view(
 BASE_API_URL = "api/v1"
 
 urlpatterns = [
-    re_path(r"^$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path(
-        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("admin/", admin.site.urls),
-    # path(
-    #     f"{BASE_API_URL}/users/",
-    #     include(("apps.users.routes", "apps.users")),
-    #     name="users",
-    # ),
-    # path(
-    #     f"{BASE_API_URL}/events/",
-    #     include(("apps.events.routes", "apps.events")),
-    #     name="events",
-    # ),
-]
+                  re_path(r"^$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+                  path(
+                      "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+                  ),
+                  path(
+                      "swagger/",
+                      schema_view.with_ui("swagger", cache_timeout=0),
+                      name="schema-swagger-ui",
+                  ),
+                  path("admin/", admin.site.urls),
+                  # path(
+                  #     f"{BASE_API_URL}/users/",
+                  #     include(("apps.users.routes", "apps.users")),
+                  #     name="users",
+                  # ),
+                  # path(
+                  #     f"{BASE_API_URL}/events/",
+                  #     include(("apps.events.routes", "apps.events")),
+                  #     name="events",
+                  # ),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+)
