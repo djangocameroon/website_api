b5225d9b7c6cade5d2c95ea5b3702d14a66799b3
Steve Yonkeu <yokwejuste@yahoo.com>
2024-01-29 12:22:51 +0100

    feat: updates to liecense and docs

--- website_api/settings/base.py
+++ website_api/settings/base.py
@@ -133,6 +133,10 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+# Media files (Images, Videos, etc)
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
