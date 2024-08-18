190d2e610c4449e1aa29a4044202fe12c621e432
Steve Yonkeu <yokwejuste@yahoo.com>
2024-08-18 20:23:38 +0100

    feat: adding debug toolbar

--- website_api/settings/base.py
+++ website_api/settings/base.py
@@ -70,12 +70,12 @@ WSGI_APPLICATION = "website_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("PORT"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
