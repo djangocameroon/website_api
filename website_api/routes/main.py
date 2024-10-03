cdd05e787df49b7aac6bf4a1b3fb16a88833e312
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2024-10-03 10:48:17 +0000

    blog urls and ins

--- website_api/routes/main.py
+++ website_api/routes/main.py
@@ -7,22 +7,18 @@ from django.urls import path, include
 from .swagger import urlpatterns as swagger_urlpatterns
 
 BASE_API_URL = "api/v1"
-blog_urls = [
-   path('blog/', include('blog.urls')),
 
-
-]
 
 urlpatterns = (
-        [
-            path("admin/", admin.site.urls),
+    [       path("admin/", admin.site.urls),
             path('__debug__/', include(debug_toolbar.urls)),
             path(f"{BASE_API_URL}/", include("apps.users.routes.api")),
             path(f"{BASE_API_URL}/", include("apps.events.routes.api")),
             path(f"{BASE_API_URL}/", include("apps.events.routes.extra")),
-        ] + swagger_urlpatterns
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+            path(f"{BASE_API_URL}/", include("apps.blog.routes.api")),
+    ] + swagger_urlpatterns
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
 
 handler404 = "apps.users.views.index.page_not_found_view"
--- website_api/settings/base.py
+++ website_api/settings/base.py
@@ -22,9 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework.authtoken',
-    
-    
-    
+
 ]
 
 INSTALLED_APPS += THIRD_PARTY_APPS
